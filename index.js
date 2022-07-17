5a1d4f9394bbb50f2867189718c0ca28684c3ff1
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-17 11:53:37 +0600

    Routes parameter and query parameter done

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000 ;
 app.get('/', (req, res) =>{
-    res.send("Hello World")
+    res.send("Hello World !!! Can you here me ?")
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3,4,5,6,7,8])
+    res.send([1,2,3,4,5,6,7,8,9])
 })
 
-app.listen(3000, () => {
-    console.log(`My express server is running on port 3000.`)
+// Route Parameters
+app.get('/api/courses/:id', (req, res)=>{
+    res.send(req.params.id)
+})
+
+// Multiple Route Parameters
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+}) 
+// Query Parameters
+app.get('/api/post/:year/:month', (req, res) => { // "/api/posts/:year/:month?sortBy=name"
+    res.send(req.query);
+}) 
+app.listen(port, () => {
+    console.log(`My express server is running on port ${port}.`)
 })
