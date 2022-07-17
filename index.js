1f803feab3f76ae5ebddde3279ddcca13e516294
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-17 11:26:49 +0600

    get request done

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ app.get('/', (req, res) =>{
     res.send("Hello World")
 });
 
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3,4,5,6,7,8])
+})
+
 app.listen(3000, () => {
     console.log(`My express server is running on port 3000.`)
 })
